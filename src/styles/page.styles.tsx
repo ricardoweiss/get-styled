0a6f6cba844edf5062cdec369e6e8a673f675b3b
Rafael Weiss <weissfrontend@gmail.com>
2023-11-19 16:31:10 -0300

    Styles for the initial components

---
+++ src/styles/page.styles.tsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import {colors, fontWeights, text} from '@/styles/styleGuide';
+
+export const Main = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  flex: 1;
+  width: 100%;
+  height: 100vh;
+  background-color: ${colors.black};
+`;
+
+export const ConvertSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  max-width: 50rem;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  flex-direction: row;
+  flex: 1;
+  width: 100%;
+  margin-top: 3rem;
+`;
+
+export const Title = styled.h1`
+  font-size: ${text.h1};
+  font-weight: ${fontWeights.bold};
+  color: ${colors.riverGreen};
+  margin-right: 1rem;
+`;
+
+export const Subtitle = styled.h2`
+  font-size: ${text.h2};
+  font-weight: ${fontWeights.bold};
+`;
+
+export const TermsAndConditions = styled.p`
+  font-size: ${text.paragraph};
+  margin-top: 3rem;
+`;
+
+export const Description = styled.p`
+  font-size: ${text.paragraph};
+  margin-top: 3rem;
+`;
+
+export const ConvertTextArea = styled.textarea`
+  width: 100%;
+  padding: 0.5rem;
+  font-size: ${text.paragraph};
+  color: ${colors.white};
+  background-color: ${colors.lightBlack};
+  border-radius: 0.25rem;
+  resize: vertical;
+  margin-top: 3rem;
+`;
+
+export const ConvertButton = styled.button`
+  width: 100%;
+  padding: 0.5rem;
+  font-size: ${text.paragraph};
+  color: ${colors.black};
+  font-weight: ${fontWeights.bold};
+  background-color: ${colors.riverGreen};
+  border-radius: 0.25rem;
+  border: none;
+  margin-top: 2rem;
+`;
+
