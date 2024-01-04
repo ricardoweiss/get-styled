498564457089e1f82008e069cd945ae49d30af52
Rafael Weiss <weissfrontend@gmail.com>
2024-01-04 08:30:25 -0300

    Going back to HTTP

--- src/hooks/useSubmitPrompt.ts
+++ src/hooks/useSubmitPrompt.ts
@@ -1,7 +1,7 @@
 export const useSubmitPrompt = () => {
     return async (prompt: string) => {
         const promptString = `Transform the following code that is using tailwind css to a code using Styled Component library using tw twin macro alognside styled components. Write only the code without imports and explanation. ${prompt}.\``;
-        return fetch('https://ec2-34-229-232-43.compute-1.amazonaws.com:8000/prompt', {
+        return fetch('http://ec2-34-229-232-43.compute-1.amazonaws.com:8000/prompt', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
