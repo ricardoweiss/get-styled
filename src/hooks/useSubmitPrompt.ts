204f296c903a2c3552afaecee234fbb97eb5360b
Ricardo Weiss <ricardohsweiss@gmail.com>
2024-01-06 12:38:52 -0300

    Update useSubmitPrompt.ts

--- src/hooks/useSubmitPrompt.ts
+++ src/hooks/useSubmitPrompt.ts
@@ -1,13 +1,12 @@
 export const useSubmitPrompt = () => {
     return async (prompt: string) => {
-        const promptString = `Transform the following code that is using tailwind css to a code using Styled Component library using tw twin macro alognside styled components. Write only the code without imports and explanation. ${prompt}.\``;
-        return fetch('http://ec2-34-229-232-43.compute-1.amazonaws.com:8000/prompt', {
+        return fetch('https://api.cssconvertor.com/prompt', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                "prompt": promptString
+                "prompt": prompt
             })
         })
     }
