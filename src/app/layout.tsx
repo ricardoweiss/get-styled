766f6284731baa6d13610acdfd8fd9a9cf5248ff
Rafael Weiss <weissfrontend@gmail.com>
2024-01-07 17:55:55 -0300

    New way of implementing ads script inside the head tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,30 @@
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import StyledComponentsRegistry from '@/lib/registry';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
- children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
     <head>
-    {process.env.NODE_ENV === 'production' && (
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127168004839495"
-              crossOrigin="anonymous"></script>
-    )}
+      {/*{process.env.NODE_ENV === 'production' && (*/}
+      {/*  <script async*/}
+      {/*          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127168004839495"*/}
+      {/*          crossOrigin="anonymous"></script>*/}
+      {/*)}*/}
+      <Script
+        id="Absence-banner"
+        async
+        strategy="afterInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127168004839495"
+        crossOrigin="anonymous"
+      />
       <title>
         CSS Convertor
       </title>
