6b31cdde55be1f022c289216cbb00ed51b7bba66
Rafael Weiss <weissfrontend@gmail.com>
2024-01-07 10:32:30 -0300

    Head tag for google ads

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/styles/globals.css'
 import StyledComponentsRegistry from '@/lib/registry';
-import Head from 'next/head';
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,18 +12,19 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({
-  children,
+ children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <Head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
-      <body className={inter.className}>
-        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
-      </body>
+    <Head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4127168004839495"
+              crossOrigin="anonymous"></script>
+    </Head>
+    <body className={inter.className}>
+    <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+    </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
               placeholder="Paste your code here"
             />
             <ConvertButton loading={loading} onClick={convert}>
-              Start translating now
+              Start translating
             </ConvertButton>
           </TextAreaBlock>
           <Spacing/>
